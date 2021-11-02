1fa910b0f7a528c7921585eaf27de66430b369e0
Matt ✔️ <matsavaqe26@gmail.com>
2021-11-01 20:00:01 -0500

    Added seeds

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: "Super secret secret",
-  cookie: {},
+  secret: "Cookies yummy",
+  cookie: {
+    maxAge: 86400,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
