a31e05b36f163fda5e57acbd8af5ea8b7c385e3d
Matt ✔️ <matsavaqe26@gmail.com>
2021-11-01 20:29:56 -0500

    Added handlebar code

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: "Cookies yummy",
+  secret: "cookies yummy",
   cookie: {
     maxAge: 86400,
   },
