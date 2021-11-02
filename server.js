af215f731ba13db8f6dc4400e799d71a982c03fb
unknown <amery.bur410@gmail.com>
2021-11-01 20:30:08 -0500

    adding stuff.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
+//const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 
 const app = express();
@@ -20,6 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+//sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
-});
+//});
