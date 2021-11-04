122617dd92a54c756544d5d2cea3f45a1f209537
Matt ✔️ <matsavaqe26@gmail.com>
2021-11-03 21:32:13 -0500

    Fixed login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,35 +2,23 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
-// router.get("/", withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ["password"] },
-//       order: [["name", "ASC"]],
-//     });
-
-router.get("/", async (req, res) => {
-  res.render("homepage", { test: "test" });
+router.get("/", withAuth, async (req, res) => {
+  try {
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-//     const users = userData.map((project) => project.get({ plain: true }));
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
 
-//     res.render("homepage", {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/login", (req, res) => {
-//   if (req.session.logged_in) {
-//     res.redirect("/");
-//     return;
-//   }
-
-//   res.render("login");
-// });
+  res.render("login");
+});
 
 module.exports = router;
