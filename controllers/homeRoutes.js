91921a1202eba3c82a8a6773d32e864af6f71ac2
unknown <amery.bur410@gmail.com>
2021-11-06 11:17:05 -0500

    updating my branch and adding seed data for electronics.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,17 +2,6 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
-<<<<<<< HEAD
-// router.get("/", withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ["password"] },
-//       order: [["name", "ASC"]],
-//     });
-
-router.get("/", async (req, res) => {
-  res.render("homepage", { test: "test" });
-=======
 router.get("/", withAuth, async (req, res) => {
   try {
     res.render("homepage", {
@@ -21,27 +10,15 @@ router.get("/", withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
->>>>>>> 9cc1c87d7ad3ee0e78285f6c4cf7e3bf0c5ebfe8
 });
 
-//     const users = userData.map((project) => project.get({ plain: true }));
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
 
-//     res.render("homepage", {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/login", (req, res) => {
-//   if (req.session.logged_in) {
-//     res.redirect("/");
-//     return;
-//   }
-
-//   res.render("login");
-// });
+  res.render("login");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -26,13 +26,16 @@ const sess = {
 };
 
 app.use(session(sess));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(routes);
-//sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-//});
 
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
