6a2f00e85b1b4a0755c8fd9700cbd2c193c44930
Matt ✔️ <matsavaqe26@gmail.com>
2021-11-06 10:45:33 -0500

    added logout feature

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("Failed to log in.");
+      console.log("Failed to login");
     }
   }
 };
@@ -36,7 +36,7 @@ const signupFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("Failed to sign up.");
+      console.log("Failed to sign up");
     }
   }
 };
