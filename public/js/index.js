d371f71e5a328b98cc1400e33f4bb5d52ccde8ce
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-11-08 21:03:11 -0600

    styled cards more

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
   $(".sidenav").sidenav();
 });
+
+$(".BuyForm .AddToCart").on("click", function (event) {
+  event.preventDefault();
+
+  var $form = $(this).closest(".BuyForm");
+
+  $.ajax({
+    type: "POST",
+    url: "/cart",
+
+    data: $form.serializeObject(),
+    success: function () {
+      $.ajax({
+        type: "GET",
+        url: "/interface/MiniCart",
+        success: function (data) {
+          $(".MiniCartContainer").html(data);
+        },
+      });
+    },
+  });
+});
