f62302f4e7daf0857aad8b4a26e9ea50cec70389
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-11-08 21:58:44 -0600

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -2,24 +2,24 @@ $(document).ready(function () {
   $(".sidenav").sidenav();
 });
 
-$(".btn").on("click", function (event) {
-  event.preventDefault();
+// $(".btn").on("click", function (event) {
+//   event.preventDefault();
 
-  var $form = $(this).closest(".card");
+//   var $form = $(this).closest(".card");
 
-  $.ajax({
-    type: "POST",
-    url: "/cart",
+//   $.ajax({
+//     type: "POST",
+//     url: "/cart",
 
-    data: $form.serializeObject(),
-    success: function () {
-      $.ajax({
-        type: "GET",
-        url: "/interface/MiniCart",
-        success: function (data) {
-          $(".MiniCartContainer").html(data);
-        },
-      });
-    },
-  });
-});
+//     data: $form.serializeObject(),
+//     success: function () {
+//       $.ajax({
+//         type: "GET",
+//         url: "/interface/MiniCart",
+//         success: function (data) {
+//           $(".MiniCartContainer").html(data);
+//         },
+//       });
+//     },
+//   });
+// });
